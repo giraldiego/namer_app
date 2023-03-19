cb2babafc2541ff15d5a7d4af625c1a0b0904cd1
giraldiego <giraldiego@gmail.com>
2023-03-19 13:46:57 -0500

    Improve accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,10 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase, 
+          style: style,
+          semanticsLabel: '${pair.first} ${pair.second}',),
       ),
     );
   }
